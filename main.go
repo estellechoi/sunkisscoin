61624f142a5912e96251fe864f018df67344ad9c
Estelle Choi <estele.choi@gmail.com>
2022-01-26 22:51:02 +0900

    feature: webserver in draft

--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/estellechoi/sunkisscoin/blockchain"
+	"log"
+	"net/http"
 )
 
+const port string = ":4000"
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(rw, "Hello from home !")
+}
+
 func main() {
-	chain := blockchain.GetBlockChain()
-	chain.AddBlock("Second Block")
-	for _, block := range chain.GetAllBlocks() {
-		fmt.Printf("%s\n", block)
-	}
+	// chain := blockchain.GetBlockChain()
+	// chain.AddBlock("Second Block")
+	// for _, block := range chain.GetAllBlocks() {
+	// 	fmt.Printf("%s\n", block)
+	// }
+	http.HandleFunc("/", home)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	err := http.ListenAndServe(port, nil)
+	log.Fatal(err)
 }
